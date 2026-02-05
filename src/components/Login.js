342cd030df2aff72cc3f0059557143d3170b09f9
Aadarsha Chhetri <ad@Aadarshas-MacBook-Pro.local>
2026-02-06 00:37:50 +0545

    updated

--- src/components/Login.js
+++ src/components/Login.js
@@ -14,7 +14,7 @@ function Login({ onLogin }) {
       const { data, error } = await supabase.auth.signInWithOAuth({
         provider: 'google',
         options: {
-          redirectTo: window.location.origin
+          redirectTo: `${window.location.origin}`
         }
       });
       
